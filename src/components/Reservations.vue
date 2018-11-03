1b83c79987dd14a29bd07b79ec2aa7e2225f92c4
Mike <mikew7224@gmail.com>
2018-11-03 17:26:20 -0500

    created reservations page -added forms for user to input first name, last name, and phone number -added check in and check out selection -added confirm reservation button -added eslint files and fixes

--- src/components/Reservations.vue
+++ src/components/Reservations.vue
@@ -1,26 +1,100 @@
 <template>
   <v-app>
-    <v-card>
+    <v-container>
+      <v-toolbar dark color="cyan">
+        <v-toolbar-title>Reservations</v-toolbar-title>
+      </v-toolbar>
+
     <v-form ref="form" v-model="valid" lazy validation>
       <v-text-field
-      v-model="name"
-      :rules="rulesForName"
-      :counter="5"
-      label="Name"
+      v-model="firstname"
+      :rules="rulesForFirstName"
+      label="First Name"
       required>
     </v-text-field>
+    <v-text-field
+    v-model="lastname"
+    :rules="rulesForLastName"
+    label="Last Name"
+    required>
+  </v-text-field>
+    <v-text-field
+    v-model="phonenumber"
+    :rules="rulesForPhone"
+    label="Phone Number"
+    required>
+  </v-text-field>
   </v-form>
+  <v-menu
+    ref="menu"
+    :close-on-content-click="false"
+    v-model="menu"
+    :nudge-right="50"
+    lazy
+    transition="scale-transition"
+    offset-x
+    full-width
+    min-width="300px">
+    <v-text-field
+    slot="activator"
+    v-model="date"
+    label="Choose a Check-In Date"
+    prepend-icon="event"
+    readonly>
+  </v-text-field>
+  <v-date-picker v-model="date" @input="menu = false">
+    <v-spacer></v-spacer>
+  </v-date-picker>
+</v-menu>
+<v-menu
+  ref="menu2"
+  :close-on-content-click="false"
+  v-model="menu2"
+  :nudge-right="50"
+  lazy
+  transition="scale-transition"
+  offset-x
+  full-width
+  min-width="300px">
+  <v-text-field
+  slot="activator"
+  v-model="date2"
+  label="Choose a Check-Out Date"
+  prepend-icon="event"
+  readonly>
+</v-text-field>
+<v-date-picker v-model="date2" @input="menu2 = false">
+  <v-spacer></v-spacer>
+</v-date-picker>
+</v-menu>
+<v-btn dark color="cyan">Confirm reservation</v-btn>
+</v-container>
   </v-app>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-
-    }
+  data: ()=> ({
+    valid: true,
+    firstname: '',
+    rulesForFirstName: [
+      v => !!v || "First name is required",
+    ],
+    lastname: '',
+    rulesForLastName: [
+      v => !!v || "Last name is required",
+    ],
+    phonenumber: '',
+    rulesForPhone: [
+      v => !!v || 'Phone number is required',
+    ],
+    date: new Date().toISOString().substr(0,10),
+    date2: new Date().toISOString().substr(0,10),
+    menu: false,
+    modal: false,
+    menu2: false
+    }),
   }
-}
 </script>
 
 <style scoped>
