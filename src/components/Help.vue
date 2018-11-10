b8d2b061254433d81c21aed40aa4fbcbe15c4692
BCRevels <31749493+BCRevels@users.noreply.github.com>
2018-11-10 10:58:59 -0600

    Add more Help content

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -11,10 +11,27 @@
         column
         justify-center
       >
-        <h1 class="display-2 font-weight-thin mb-3">Vuetify.js</h1>
-        <h4 class="subheading">Build your application today!</h4>
+        <h1 class="display-3 font-weight-medium text-capitalize mb-3">Need Some Help?</h1>
+        <h4 class="display-1 text-capitalize">Check out some useful information below</h4>
       </v-layout>
     </v-parallax>
+    <p style="margin-top: 10px"></p>
+    <v-layout column>
+      <v-card-text>
+        <h1 class="title font-weight-medium text-capitalize text-style-underline">About Us:</h1>
+        <p class="subheading font-weight-regular">
+          We at Hotelify pride ourselves in providing the highest quality rooms on this side of the Mississippi River.<br/>
+          Your satisfaction is guaranteed!
+        </p>
+        <hr/>
+      </v-card-text>
+      <v-card-text>
+        <h1 class="title font-weight-medium text-capitalize text-style-underline">Contact Us:</h1>
+        <p class="subheading font-weight-regular">Phone:</p>
+        <p class="subheading font-weight-regular">Address:</p>
+        <hr/>
+      </v-card-text>
+    </v-layout>
   </v-app>
 </template>
 
@@ -26,9 +43,11 @@
       }
     }
   }
-
 </script>
 
 <style scoped>
-
+  .text-style-underline{
+    text-decoration: underline;
+    margin-bottom: 4px;
+  }
 </style>
