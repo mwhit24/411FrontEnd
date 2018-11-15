8b5cdab21c84411cd196435b906ba2150896e82e
BCRevels <31749493+BCRevels@users.noreply.github.com>
2018-11-14 21:26:57 -0600

    Add axios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,9 @@
     <h3><strong>Welcome to Hotelify!</strong></h3>
     <p></p>
     <p>We are glad that you chose to use Hotelify for your hotel management needs. We have many options available, and they are located in the toolbar.
-      If you have any questions, let our team know. Thank you for using Hotelify!</p>
+      If you have any questions, let our team know. Thank you for using Hotelify!<br/>
+      {{info}}
+    </p>
 
     </v-card>
   </v-container>
@@ -15,11 +17,17 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  data() {
+  data () {
     return {
-
+      info: null
     }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
